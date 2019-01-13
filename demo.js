135a2ebf2cbd2936b1c47bb455a75db0b18a25be
Bingco <ittat@live.com>
2019-01-13 04:00:22 +0000

    Add files via upload

--- demo.js
+++ demo.js
@@ -3,17 +3,51 @@ var exec = require('child_process').exec;
 var arguments = process.argv.splice(2);
 console.log('所传递的参数是：', arguments);
 
+var home_path = process.env.HOME();//???
 
 var cmdStr = '';
-process.argv.forEach(function (val, index, array) {
+arguments.forEach(function (val, index, array) {
   cmdStr = cmdStr + ' ' + val ;
 });
 
-
-exec(cmdStr, function(err,stdout,stderr){
-    if(err) {
-        console.log('error:'+stderr);
-    } else {
-	console.log('stdout:'+stdout);
-    }
+function Cmd(cmdStr){
+	exec(cmdStr, function(err,stdout,stderr){
+   	 if(err) {
+       		console.log('error:'+stderr);
+  	  } else {
+		console.log('stdout:'+stdout);
+  	  }
 });
+
+}
+
+
+function type_prompt(home_path){
+	var head = '[node-shell:]';
+	var path = process.cwd();
+	if( path == home_path ){
+		head = head + '~' + '#' + ' ' ;
+	}else {
+		head = head + path + '#'+ ' ' ;
+	}
+	return head;
+}
+
+
+function read_command(){
+	if( process.argv.val[0] == 'cd' ){
+		var arguments_tmp = process.argv.splice(1);
+		var cmdStr_tmp = '';
+		arguments_tmp.forEach(function (val, index, array) {
+		  cmdStr_tmp = cmdStr_tmp + ' ' + cmdStr_tmp ;
+		});
+		console.log('所传递的参数是：', cmdStr_tmp);
+		process.chdir(cmdStr_tmp);
+	}else {
+		console.log('所传递的参数是：', cmdStr);
+		Cmd(cmdStr);
+	}
+	
+}
+
+
